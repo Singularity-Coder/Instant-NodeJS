21ef6e7f5ea054d0e4a04c799ea9c81dcf08305e
Singularity Coder <singularitycoder@Singularitys-MacBook-Air.local>
2022-08-29 00:53:43 +0530

    More routes

--- Tests/MyRestApi/app.js
+++ Tests/MyRestApi/app.js
@@ -14,6 +14,35 @@ const app = express()
 const productRoute = require("./routes/product.route")
 app.use("/products", productRoute)
 
+// Middleware: When the above operation fails it comes here. Used for handling invalid requests, sending error responses, etc
+// If this is not there then a default HTML error is sent to user. Not good UX.
+// All routes that are not handled by our App is handled by this middleware.
+app.use((req, res, next) => {
+    // res.status(404)
+    // res.send({
+    //     error: "Not found"
+    // })
+
+    // Creating custom error
+    const err = new Error("Not found")
+    err.status = 404
+
+    // Use next object to pass the above error to the Error Handler below
+    // This calls the operation below. U can call it from any route
+    next(err)
+})
+
+// Express Error Handler: Middleware
+app.use((err, req, res, next) => {
+    res.status(err.status || 500) // If status is missing then default to 500 internal server error
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+})
+
 // Start the server on port 3000 and get the callback
 app.listen(3000, () => {
     console.log("Server started on port 3000")
--- Tests/MyRestApi/routes/product.route.js
+++ Tests/MyRestApi/routes/product.route.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const router = express.Router()
 
-// GET Products route
+// GET Product List: http://localhost:3000/products
 router.get('/', (req, res, next) => {
     res.send("getting a list of all products...")
+})
+
+// POST Product: http://localhost:3000/create_product
+router.post('/create_product', (req, res, next) => {
+    res.send("creating an item")
+})
+
+// GET Product by ID: http://localhost:3000/products/id
+router.get('/:id', (req, res, next) => {
+    res.send("getting an item")
+})
+
+// PATCH Product by ID: http://localhost:3000/products/id
+router.patch('/:id', (req, res, next) => {
+    res.send("updating an item")
+})
+
+// DELETE Product by ID: http://localhost:3000/products/id
+router.delete('/:id', (req, res, next) => {
+    res.send("deleting an item")
+})
+
+// POST Product by ID: http://localhost:3000/products/create_category
+router.post('/create_category', (req, res, next) => {
+    next(new Error("cannot create category. Something went wrong"))
 })
 
 module.exports = router
